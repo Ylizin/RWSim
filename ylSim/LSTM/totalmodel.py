8b727c164c7d2b05af7aca964803242908627ef3
ylizin <ylizin@163.com>
2019-04-16 09:50:22 +0800

    restore params

--- ylSim/LSTM/totalmodel.py
+++ ylSim/LSTM/totalmodel.py
@@ -9,11 +9,11 @@ class RWLSTMModel(nn.Module):
         super().__init__()
         self.lstm = RWLSTM.RWLSTM(args)
         self.bi = Sim.CalSim(args)
-        self.relu  = nn.ReLU()
+        # self.relu  = nn.ReLU()
         
     def forward(self, inseq1 , inseq2):
         output1,h1,c1,input1 = self.lstm(inseq1)
         output2,h2,c2,input2 = self.lstm(inseq2)
-        return self.relu(self.bi(output1,output2))
+        return self.bi(output1,output2)
 
     
