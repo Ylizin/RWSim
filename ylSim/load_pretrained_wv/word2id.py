e4dcb8322f4b2e9f829039afc5a362c03d0c57b4
yl_xps <ylizin@163.com>
2019-04-17 20:56:12 +0800

    tune tune

--- ylSim/load_pretrained_wv/word2id.py
+++ ylSim/load_pretrained_wv/word2id.py
@@ -3,23 +3,24 @@ import utils
 import numpy as np
 from .save_w2v import save_w2v_w2i
 
-def load_w2vi(word2index,save_path = utils.extract_w2v_path+'.txt'):
+def load_w2vi(word2index,save_path = utils.extract_w2v_path):
     w2i = {}
-    with open(save_path+'.txt','r') as f:
-        for no,line in enumerate(f):
-            line = line.strip().split()
-            if line:
-                word = line[0]
-                index = line[1]
-                w2i[word] = int(index)
+    if os.path.exists(save_path+'.txt'):
+        with open(save_path+'.txt','r') as f:
+            for no,line in enumerate(f):
+                line = line.strip().split()
+                if line:
+                    word = line[0]
+                    index = line[1]
+                    w2i[word] = int(index)
     pret = None
-    if w2i != word2index:  # if the corpus updated ,the we need to regenerate the npy
+    if w2i != word2index:  # if the corpus updated ,the we need to regenerate the npy,here should be completed
         pret = save_w2v_w2i(word2index)
     else:
         pret = __load_w2v()
     return w2i,pret
 
-def __load_w2v(save_path = utils.extract_w2v_path+'.txt'):
+def __load_w2v(save_path = utils.extract_w2v_path):
     w2v = np.load(save_path+'.npy')
     return w2v
 
