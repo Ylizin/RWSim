ba0a234eceefa93857dcd31132936d25a814ce24
yl_xps <ylizin@163.com>
2019-05-19 20:27:20 +0800

    tune

--- ylSim/TMN/TMNModel.py
+++ ylSim/TMN/TMNModel.py
@@ -75,7 +75,7 @@ class TMNModel(nn.Module):
         
         joint_match = self.softmax(torch.add(theta,match)).unsqueeze(1) # this will be the sum topic of theta and word in doc,(bzs,k)
         # joint_match = torch.add(theta.expand(self.max_length,-1,-1).transpose(0,1),match)
-        joint_match = self.relu(torch.bmm(joint_match,wt_embedding))# (bz,topic_embedding_size)
+        joint_match = self.relu(self.f1(joint_match))# (bz,topic_embedding_size)
         _feature_strengthed = torch.add(torch.sum(feature_input,dim=1).unsqueeze(1),joint_match)
         feature_input = _feature_strengthed
         # match = torch.bmm(feature_input,wt_embedding.transpose(1,2))# match will be (bz,L,K)
